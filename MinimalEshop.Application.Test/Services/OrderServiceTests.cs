1249cfb7474805b16d626d570836fc9c0afe9d09
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2025-10-29 22:52:03 +0530

    Added Getorderdetails unit test.

--- MinimalEshop.Application.Test/Services/OrderServiceTests.cs
+++ MinimalEshop.Application.Test/Services/OrderServiceTests.cs
@@ -20,7 +20,7 @@ namespace MinimalEshop.Tests.Service
 
         [Fact]
         public async Task CheckOutAsync_ShouldReturnExpectedResult()
-        {
+            {
             var userId = "testUser123";
             var expectedResult = (true, "Checkout successful", new { OrderId = "OID123" });
 
@@ -34,11 +34,11 @@ namespace MinimalEshop.Tests.Service
             Assert.NotNull(result.data);
 
             _mockOrderRepo.Verify(repo => repo.CheckOutAsync(userId), Times.Once);
-        }
+            }
 
         [Fact]
         public async Task ProcessPaymentAsync_ShouldReturnExpectedResult()
-        {
+            {
             var userId = "testUser123";
             var paymentMethod = PaymentMethod.UPI;
             var expectedResult = (true, "Payment processed successfully");
@@ -53,6 +53,22 @@ namespace MinimalEshop.Tests.Service
             Assert.Equal("Payment processed successfully", result.message);
 
             _mockOrderRepo.Verify(repo => repo.ProcessPaymentAsync(userId, paymentMethod), Times.Once);
+            }
+
+        [Fact]
+        public async Task GetOrderDetailsAsync_Should_Call_Repository_And_Return_Result()
+            {
+            var userId = "user123";
+            var expectedResult = (true, "Order details fetched successfully", new { OrderId = "ORD123" });
+
+            _mockOrderRepo
+                .Setup(r => r.GetOrderDetailsAsync(userId))
+                .ReturnsAsync(expectedResult);
+
+            var result = await _orderService.GetOrderDetailsAsync(userId);
+
+            Assert.Equal(expectedResult, result);
+            _mockOrderRepo.Verify(r => r.GetOrderDetailsAsync(userId), Times.Once);
         }
     }
 }
