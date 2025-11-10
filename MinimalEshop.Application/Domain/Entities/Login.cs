866f099980920a69b98f66c80feef21aa1ec894d
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2025-11-10 14:28:47 +0530

    Login entity added.

---
+++ MinimalEshop.Application/Domain/Entities/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalEshop.Application.Domain.Entities
+    {
+    public class Login
+        {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        }
+
+    }
